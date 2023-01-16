e98975d0416be47c34d4a24d59a97c573fb17ea3
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-01-16 18:39:24 +0100

    Creating subclass structure

---
+++ data_models/GrassPokemon.py
@@ -0,0 +1,11 @@
+from data_models.Pokemon import Pokemon
+
+
+class GrassPokemon(Pokemon):
+    def __init__(self, name: str, pokedexId: str, level: int, living_points: int, attacking_points: int,
+                 defense_points: int, attack):
+        super().__init__(name, pokedexId, level, living_points, attacking_points,
+                         defense_points, attack)
+
+    def lvlUp(self) -> Pokemon:
+        pass
--- data_models/Pokemon.py
+++ data_models/Pokemon.py
@@ -1,11 +1,12 @@
+from __future__ import annotations
 from typing import Tuple, List
+from abc import ABC, abstractmethod
 
-
-class Pokemon():
-    def __init__(self,name: str, pokedexId: str, level: int, living_points: int, attacking_points: int,
+class Pokemon(ABC):
+    def __init__(self, name: str, pokedexId: str, level: int, living_points: int, attacking_points: int,
                  defense_points: int, attack: List[Tuple[str, int]]) -> None:
         self.name = name
-        self.pokedesId = pokedexId
+        self.pokedexId = pokedexId
         self.level = level
         self.living_points = living_points
         self.attacking_points = attacking_points
@@ -14,3 +15,7 @@ class Pokemon():
         self.experience_points: int = 0
         self.fight_status: bool = False
         self.alive: bool = True
+
+    @abstractmethod
+    def lvlUp(self) -> Pokemon:
+        pass
